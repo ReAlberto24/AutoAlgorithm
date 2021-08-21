b0bd8c9cc0789328296713d2b383bf3db5459b49
ReAlberto24 <albert.to.trebla2@gmail.com>
2021-08-21 22:51:53 +0200

    V 1.2.0

--- project/main.py
+++ project/main.py
@@ -1,5 +1,4 @@
-import os, screeninfo, pyautogui, pytesseract, re, time, mouse, threading
-import keyboard
+import os, screeninfo, pyautogui, pytesseract, re, time, mouse, threading, keyboard, traceback
 from playsound import playsound
 
 def checkIfProcessRunning(processName):
@@ -48,148 +47,150 @@ except: pass
 runAFK = False
 
 def algC():
-    try:
-        global runAFK
-        runAFK = False
-        bestAlg = ''
+    
+    global runAFK
+    runAFK = False
+    bestAlg = ''
 
-        # SHA-512
-        SHA512 = pyautogui.screenshot(region=(1600, 655, 180, 35))
+    while True:
+        try:
+            # SHA-512
+            SHA512 = pyautogui.screenshot(region=(1400, 655, 300, 35))
 
-        # Dagger
-        Dagger = pyautogui.screenshot(region=(1500, 705, 300, 36))
+            # Dagger
+            Dagger = pyautogui.screenshot(region=(1400, 705, 300, 36))
 
-        # Scrypt
-        Scrypt = pyautogui.screenshot(region=(1500, 758, 300, 36))
+            # Scrypt
+            Scrypt = pyautogui.screenshot(region=(1400, 758, 300, 36))
 
-        # Bloxchain
-        Bloxchain = pyautogui.screenshot(region=(1500, 813, 300, 36))
-        
-        SHA512.save(f'algDir\\SHA512.png')
-        Dagger.save(f'algDir\\Dagger.png')
-        Scrypt.save(f'algDir\\Scrypt.png')
-        Bloxchain.save(f'algDir\\Bloxchain.png')
+            # Bloxchain
+            Bloxchain = pyautogui.screenshot(region=(1400, 813, 300, 36))
+            
+            SHA512.save(f'algDir\\SHA512.png')
+            Dagger.save(f'algDir\\Dagger.png')
+            Scrypt.save(f'algDir\\Scrypt.png')
+            Bloxchain.save(f'algDir\\Bloxchain.png')
 
-        algList = []
+            algList = []
 
-        # SHA-512
-        SHA512V = pytesseract.image_to_string(f'algDir\\SHA512.png')
-        SHA512VL = re.findall(r'\d+', SHA512V)
-        SHA512VS = SHA512VL[0]+SHA512VL[1]
-        SHA512VS2 = SHA512VL[0]+'.'+SHA512VL[1]+'x'
-        algList.append(SHA512VS)
+            # SHA-512
+            SHA512V = pytesseract.image_to_string(f'algDir\\SHA512.png')
+            SHA512VL = re.findall(r'\d+', SHA512V)
+            SHA512VS = SHA512VL[1]+SHA512VL[2]
+            SHA512VS2 = SHA512VL[1]+'.'+SHA512VL[2]+'x'
+            algList.append(SHA512VS)
 
-        # Dagger
-        DaggerV = pytesseract.image_to_string(f'algDir\\Dagger.png')
-        DaggerVL = re.findall(r'\d+', DaggerV)
-        DaggerVS = DaggerVL[0]+DaggerVL[1]
-        DaggerVS2 = DaggerVL[0]+'.'+DaggerVL[1]+'x'
-        algList.append(DaggerVS)
+            # Dagger
+            DaggerV = pytesseract.image_to_string(f'algDir\\Dagger.png')
+            DaggerVL = re.findall(r'\d+', DaggerV)
+            DaggerVS = DaggerVL[0]+DaggerVL[1]
+            DaggerVS2 = DaggerVL[0]+'.'+DaggerVL[1]+'x'
+            algList.append(DaggerVS)
 
-        # Scrypt
-        ScryptV = pytesseract.image_to_string(f'algDir\\Scrypt.png')
-        ScryptVL = re.findall(r'\d+', ScryptV)
-        ScryptVS = ScryptVL[0]+ScryptVL[1]
-        ScryptVS2 = ScryptVL[0]+'.'+ScryptVL[1]+'x'
-        algList.append(ScryptVS)
+            # Scrypt
+            ScryptV = pytesseract.image_to_string(f'algDir\\Scrypt.png')
+            ScryptVL = re.findall(r'\d+', ScryptV)
+            ScryptVS = ScryptVL[0]+ScryptVL[1]
+            ScryptVS2 = ScryptVL[0]+'.'+ScryptVL[1]+'x'
+            algList.append(ScryptVS)
 
-        # Bloxchain
-        BloxchainV = pytesseract.image_to_string(f'algDir\\Bloxchain.png')
-        BloxchainVL = re.findall(r'\d+', BloxchainV)
-        BloxchainVS = BloxchainVL[0]+BloxchainVL[1]
-        BloxchainVS2 = BloxchainVL[0]+'.'+BloxchainVL[1]+'x'
-        algList.append(BloxchainVS)
-        
-        #print(SHA512VS, DaggerVS, ScryptVS, BloxchainVS)
+            # Bloxchain
+            BloxchainV = pytesseract.image_to_string(f'algDir\\Bloxchain.png')
+            BloxchainVL = re.findall(r'\d+', BloxchainV)
+            BloxchainVS = BloxchainVL[0]+BloxchainVL[1]
+            BloxchainVS2 = BloxchainVL[0]+'.'+BloxchainVL[1]+'x'
+            algList.append(BloxchainVS)
+            
+            #print(SHA512VS, DaggerVS, ScryptVS, BloxchainVS)
 
-        if SHA512VS == max(algList):
-            if bestAlg != 'SHA-512':
-                print('\n')
-                print('Il miglior algoritmo è SHA-512 con {}'.format(SHA512VS2))
-                bestAlg = 'SHA-512'
-                if every > 2.0:
-                    mouse.move(1323, 1060, True, 0)
-                    mouse.click('left')
-                    time.sleep(0.1)
-                    mouse.move(1365, 670, True, 1)
-                    mouse.click('left')
-                    mouse.click('left')
-                else:
-                    mouse.move(1323, 1060, True, 0)
-                    mouse.click('left')
-                    time.sleep(0.1)
-                    mouse.move(1365, 670, True, 0)
-                    mouse.click('left')
-                    mouse.click('left')
+            if SHA512VS == max(algList):
+                if bestAlg != 'SHA-512':
+                    print('\n')
+                    print('Il miglior algoritmo è SHA-512 con {}'.format(SHA512VS2))
+                    bestAlg = 'SHA-512'
+                    if every > 2.0:
+                        mouse.move(1323, 1060, True, 0)
+                        mouse.click('left')
+                        time.sleep(0.1)
+                        mouse.move(1365, 670, True, 1)
+                        mouse.click('left')
+                        mouse.click('left')
+                    else:
+                        mouse.move(1323, 1060, True, 0)
+                        mouse.click('left')
+                        time.sleep(0.1)
+                        mouse.move(1365, 670, True, 0)
+                        mouse.click('left')
+                        mouse.click('left')
 
-        elif DaggerVS == max(algList):
-            if bestAlg != 'Dagger':
-                print('\n')
-                print('Il miglior algoritmo è Dagger con {}'.format(DaggerVS2))
-                bestAlg = 'Dagger'
-                if every > 2.0:
-                    mouse.move(1323, 1060, True, 0)
-                    mouse.click('left')
-                    time.sleep(0.1)
-                    mouse.move(1365, 725, True, 1)
-                    mouse.click('left')
-                    mouse.click('left')
-                else:
-                    mouse.move(1323, 1060, True, 0)
-                    mouse.click('left')
-                    time.sleep(0.1)
-                    mouse.move(1365, 725, True, 0)
-                    mouse.click('left')
-                    mouse.click('left')
+            elif DaggerVS == max(algList):
+                if bestAlg != 'Dagger':
+                    print('\n')
+                    print('Il miglior algoritmo è Dagger con {}'.format(DaggerVS2))
+                    bestAlg = 'Dagger'
+                    if every > 2.0:
+                        mouse.move(1323, 1060, True, 0)
+                        mouse.click('left')
+                        time.sleep(0.1)
+                        mouse.move(1365, 725, True, 1)
+                        mouse.click('left')
+                        mouse.click('left')
+                    else:
+                        mouse.move(1323, 1060, True, 0)
+                        mouse.click('left')
+                        time.sleep(0.1)
+                        mouse.move(1365, 725, True, 0)
+                        mouse.click('left')
+                        mouse.click('left')
 
-        elif ScryptVS == max(algList):
-            if bestAlg != 'Scrypt':
-                print('\n')
-                print('Il miglior algoritmo è Scrypt con {}'.format(ScryptVS2))
-                bestAlg = 'Scrypt'
-                if every > 2.0:
-                    mouse.move(1323, 1060, True, 0)
-                    mouse.click('left')
-                    time.sleep(0.1)
-                    mouse.move(1365, 775, True, 1)
-                    mouse.click('left')
-                    mouse.click('left')
-                else:
-                    mouse.move(1323, 1060, True, 0)
-                    mouse.click('left')
-                    time.sleep(0.1)
-                    mouse.move(1365, 775, True, 0)
-                    mouse.click('left')
-                    mouse.click('left')
+            elif ScryptVS == max(algList):
+                if bestAlg != 'Scrypt':
+                    print('\n')
+                    print('Il miglior algoritmo è Scrypt con {}'.format(ScryptVS2))
+                    bestAlg = 'Scrypt'
+                    if every > 2.0:
+                        mouse.move(1323, 1060, True, 0)
+                        mouse.click('left')
+                        time.sleep(0.1)
+                        mouse.move(1365, 775, True, 1)
+                        mouse.click('left')
+                        mouse.click('left')
+                    else:
+                        mouse.move(1323, 1060, True, 0)
+                        mouse.click('left')
+                        time.sleep(0.1)
+                        mouse.move(1365, 775, True, 0)
+                        mouse.click('left')
+                        mouse.click('left')
 
-        elif BloxchainVS == max(algList):
-            if bestAlg != 'Bloxchain':
-                print('\n')
-                print('Il miglior algoritmo è Bloxchain con {}'.format(BloxchainVS2))
-                bestAlg = 'Bloxchain'
-                if every > 2.0:
-                    mouse.move(1323, 1060, True, 0)
-                    mouse.click('left')
-                    time.sleep(0.1)
-                    mouse.move(1365, 830, True, 1)
-                    mouse.click('left')
-                    mouse.click('left')
-                else:
-                    mouse.move(1323, 1060, True, 0)
-                    mouse.click('left')
-                    time.sleep(0.1)
-                    mouse.move(1365, 830, True, 0)
-                    mouse.click('left')
-                    mouse.click('left')
+            elif BloxchainVS == max(algList):
+                if bestAlg != 'Bloxchain':
+                    print('\n')
+                    print('Il miglior algoritmo è Bloxchain con {}'.format(BloxchainVS2))
+                    bestAlg = 'Bloxchain'
+                    if every > 2.0:
+                        mouse.move(1323, 1060, True, 0)
+                        mouse.click('left')
+                        time.sleep(0.1)
+                        mouse.move(1365, 830, True, 1)
+                        mouse.click('left')
+                        mouse.click('left')
+                    else:
+                        mouse.move(1323, 1060, True, 0)
+                        mouse.click('left')
+                        time.sleep(0.1)
+                        mouse.move(1365, 830, True, 0)
+                        mouse.click('left')
+                        mouse.click('left')
 
-        else:
-            pass
-    except:
-        pass
+            else:
+                pass
+        except: traceback.print_exc()
+            
 
-    runAFK = True
+        runAFK = True
 
-    time.sleep(every)
+        time.sleep(every)
 
 algCT = threading.Thread(target=algC)
 algCT.daemon = True
